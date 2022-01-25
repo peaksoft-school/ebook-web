c2f1c63e5a2604165260a2a40d17026fe633e86b
Lay7531 <ieye7531@gmail.com>
2022-01-25 16:12:47 +0600

    save

--- src/components/HistoryOperation/HistoryOperation.js
+++ src/components/HistoryOperation/HistoryOperation.js
@@ -1,51 +1,98 @@
 import classes from './HistoryOperation.module.css'
 import { useState } from 'react';
-import { useEffect } from 'react';
+// import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
-import { useSelector } from 'react-redux';
-import { getHistoryOperationData } from '../../store/HistoryOperationSlice';
+// import { useSelector } from 'react-redux';
+// import { getHistoryOperationData } from '../../store/HistoryOperationSlice';
 const HistoryOperation = () => {
-    const history = useSelector(state => state.history.list)
-    const status = useSelector(state => console.log(state.history.status))
-    console.log(history)
+    // const history = useSelector(state => state.history.list);
+    const [location,setLocation] = useState()
     const dispatch = useDispatch()
     const [colorOrangeBtn,setColorOrangeBtn] = useState()
+    const history = [
+        {
+            "author":"Bayba",
+            "bookName":"Harry Poter",
+            "promocode":"20",
+            "discount":"20",
+            "price":"356",
+            "data":"12.09.22",
+            "state":'завершен',
+            "location":"parchased",
+            "id":1,
+        },
+        {
+            "author":"Bayba",
+            "bookName":"Harry Poter",
+            "promocode":"20",
+            "discount":"20",
+            "price":"356",
+            "data":"12.09.22",
+            "state":'завершен',
+            "location":"basket",
+            "id":2,
+        },
+        {
+            "author":"Bayba",
+            "bookName":"Harry Poter",
+            "promocode":"20",
+            "discount":"20",
+            "price":"356",
+            "data":"12.09.22",
+            "state":'завершен',
+            "location":"favorites",
+            "id":3,
+        },
+        {
+            "author":"Bayba",
+            "bookName":"Harry Poter",
+            "promocode":"20",
+            "discount":"20",
+            "price":"356",
+            "data":"12.09.22",
+            "state":'завершен',
+            "location":"favorites",
+            "id":4,
+        },
+        {
+            "author":"Bayba",
+            "bookName":"Harry Poter",
+            "promocode":"20",
+            "discount":"20",
+            "price":"356",
+            "data":"12.09.22",
+            "state":'завершен',
+            "location":"parchased",
+            "id":5,
+        },
 
-    useEffect(() => {
-      dispatch(getHistoryOperationData());
-      console.log('sddfsd')
-    }, [dispatch]);
+    ]
+    // useEffect(() => {
+    //   dispatch(getHistoryOperationData());
+    //   console.log('sddfsd')
+    // }, [dispatch]);
     const changeToParchased=()=> {
         setColorOrangeBtn('parchased')
+        setLocation('parchased')
+        showHistory()
     }
     const changeToFavorites=()=> {
         setColorOrangeBtn('favorites')
+        setLocation('favorites')
+        showHistory()
     }
     const changeToBasket=()=> {
         setColorOrangeBtn('basket')
+        setLocation('basket')
+        showHistory()
     }
-    const mapHistory=()=> {
-        if(status=== 'success'){
-            history.map((item) => {
-                return <div className={classes.bookLi}>
-                <img className={classes.historyImage} src='https://d1csarkz8obe9u.cloudfront.net/posterpreviews/action-thriller-book-cover-design-template-3675ae3e3ac7ee095fc793ab61b812cc_screen.jpg?ts=1637008457' alt='dsdfdf'/>
-                <div >
-                    <p className={classes.bookNameInformation}>{item.login}</p>
-                    <p className={classes.bookNameInformation}>Роулинг Джоан Кэтлин</p>
-                </div>
-                <p>1 шт</p>
-                <div>
-                    <p className={classes.promocode}>Промокод 20%</p>
-                    <p><span className={classes.discount}>545 c</span>345 c</p>
-                </div>
-                <p>12.12.21</p>
-                <p>Завершен</p>
-            </div>
-            })
-        }else if(status === 'loading') {
-            return <p>loading</p>
+    const showHistory=()=> {
+        if(history) {
+            history.filter((item) => item.location === location)
+              return console.log(history)
         }
     }
+    console.log(location)
   return <div className={classes.HistoryOperationContainer}>
       <div className={classes.HistoryOperationTitles}>
       <p>Очистить историю</p>
@@ -81,7 +128,23 @@ const HistoryOperation = () => {
         </button>
         </div>
           <div className={classes.BooksList}>
-              {mapHistory()}
+              {
+               history && history.map((item) => {
+                  return <ul key={item.id} className={classes.bookLi}>
+                  <img className={classes.historyImage} src='https://d1csarkz8obe9u.cloudfront.net/posterpreviews/action-thriller-book-cover-design-template-3675ae3e3ac7ee095fc793ab61b812cc_screen.jpg?ts=1637008457' alt='dsdfdf'/>
+                  <div >
+                      <p className={classes.bookNameInformation}>{item.login}</p>
+                      <p className={classes.bookNameInformation}>Роулинг Джоан Кэтлин</p>
+                  </div>
+                  <p>1 шт</p>
+                  <div>
+                      <p className={classes.promocode}>Промокод 20%</p>
+                      <p><span className={classes.discount}>545 c</span>345 c</p>
+                  </div>
+                  <p>12.12.21</p>
+                  <p>Завершен</p>
+              </ul>
+              })}
           </div>
       </div>
   </div>;
