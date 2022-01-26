38ce891dd2fb0feba029a792d8eb03fd8c01f9d3
Zaryl-Zh <zaryl.abdrazakova@gmail.com>
2022-01-26 11:38:27 +0600

    footer is being created

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import './App.css';
+import Footer from './layout/footer/Footer';
 
 function App() {
   return (
-    <div className="App"></div>
+    <div className="App">
+      <Footer/>
+    </div>
   );
 }
 
