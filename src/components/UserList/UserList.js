b2387601e41ee20f56907aa82c6637afdcc455bc
Lay7531 <ieye7531@gmail.com>
2022-01-26 12:28:51 +0600

    save

--- src/components/UserList/UserList.js
+++ src/components/UserList/UserList.js
@@ -23,11 +23,11 @@ const UserList = () => {
     //  }
   return <div className={classes.box}>
       <div className={classes.containerTitle}>
-      <p><b>№</b></p>
-      <p className={classes.mediumBoxForFIO}><b>ФИО</b></p>
-      <p className={classes.mediumBox}><b>Номер телефона</b></p>
-      <p className={classes.mediumBox}><b>Почта</b></p>
-      <p className={classes.mediumBox}><b>Количество книг</b></p>
+        <p><b>№</b></p>
+        <p className={classes.mediumBoxForFIO}><b>ФИО</b></p>
+        <p className={classes.mediumBox}><b>Номер телефона</b></p>
+        <p className={classes.mediumBox}><b>Почта</b></p>
+        <p className={classes.mediumBox}><b>Количество книг</b></p>
       </div>
       <hr className={classes.line}/>
       <div className={classes.containerList}>
