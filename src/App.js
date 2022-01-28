53deeae17b21aa5f1e5f94ab3757f372b23093cd
Lay7531 <ieye7531@gmail.com>
2022-01-28 13:49:54 +0600

    save

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
-
+import BookPage from './components/BookPage/BookPage';
 function App() {
   return (
-    <div className="App"></div>
+    <div className="App">
+      <BookPage/>
+    </div>
   );
 }
 
---
+++ src/components/BookPage/BookPage.js
@@ -0,0 +1,82 @@
+import classes from './BookPage.module.css'
+import { useState } from 'react';
+const BookPage = () => {
+  const [transiton,setTransition] = useState('about')
+  const redirectToAbout=()=> {
+    setTransition('about')
+  }
+  const redirectToFragment=()=> {
+    setTransition('fragment')
+  }
+  return <div className={classes.ContainerForBook}>
+    <div className={classes.containerForSmallInformation}>
+      <img className={classes.firstImage} src='https://pbs.twimg.com/media/EKvrgoOX0AM1_oz.jpg' alt='some images'/>
+      <img className={classes.secondImage} src='https://pbs.twimg.com/media/EKvrgoOX0AM1_oz.jpg' alt='some images'/>
+      <div className={classes.containerForInformation}>
+        <h1>Book name</h1>
+        <div className={classes.container}>
+          <p className={classes.price}>price</p>
+          <audio src='https://sgi1.beeline-kz.vkuseraudio.net/p13/a094def19932e8.mp3?extra=nLFinFtz4cKRlWhxbzu-1s0PY055QRDgwuHXwYbOMOwN2wlGg17S8eWL4lRcJL7qCo_NqDwUtyaZcAxGpbjbyqTrpPzp49zLgdL6xvlxh6-FyY9WdvFeFpNsflBIZOOplkGEs89NPX8xpEP1Tfvc12wilw' 
+          alt='there have to will be some music' />
+        </div>
+        <div className={classes.containerForBriefInformation}>
+          <div>
+            <p>Автор</p>
+            <p>Жанр</p>
+            <p>Язык</p>
+            <p>Издательство</p>
+            <p>Год выпуска</p>
+            <p>Обьем</p>
+            <p>Длительность</p>
+          </div>
+          <div className={classes.smallContainerForBriefInformation}>
+            <p>Автор</p>
+            <p>Жанр</p>
+            <p>Язык</p>
+            <p>Издательство</p>
+            <p>Год выпуска</p>
+            <p>Обьем</p>
+            <p>Длительность</p>
+          </div>
+        </div>
+        <div className={classes.containerForBtn}>
+          <button className={classes.button}>sdfsd</button>
+          <button className={classes.button}>sdfsdaf</button>
+        </div>
+      </div>
+    </div>
+    <div className={classes.containerForInformationAboutBook}>
+      <div className={classes.containerForAboutBookAndFragment}>
+        <div className={classes.containerForSwitch}>
+          <button 
+          className={transiton === 'about'? 
+          classes.redirectActiveButton 
+          : classes.redirectButton} 
+          onClick={redirectToAbout}>
+            О книге
+            </button>
+          <button 
+          className={transiton === 'fragment'? 
+          classes.redirectActiveButton 
+          : classes.redirectButton} 
+          onClick={redirectToFragment}>
+            Читать фрагмент
+            </button>
+        </div>
+        {
+          transiton === 'about' && <p className={classes.textAbout}>
+          loremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremlorem
+        </p>
+        }
+        {
+          transiton === 'fragment' && <p className={classes.textFragment}>
+          loremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremlorem
+        </p>
+        }
+      </div>
+      <img className={classes.thirdImage} src='https://pbs.twimg.com/media/EKvrgoOX0AM1_oz.jpg' alt='some images'/>
+    </div>
+  </div>;
+};
+
+export default BookPage;
