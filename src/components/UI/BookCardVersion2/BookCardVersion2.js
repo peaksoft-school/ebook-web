3d4e3471a51aed227acd1e59485f62f8fa05e262
Zaryl-Zh <zaryl.abdrazakova@gmail.com>
2022-01-30 17:18:17 +0600

    bookCard2 designed

---
+++ src/components/UI/BookCardVersion2/BookCardVersion2.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import classes from './BookCardVersion2.module.css'
+import {ReactComponent as Icon} from '../../../assets/icons/Controls Icon.svg'
+
+
+const BookCardVersion2 = ({book, onClick, className, ...restProps}) => {
+  
+
+  return <div>
+           <ul className={classes.styles}>
+            <li>
+                <div className={`${classes.card} ${className}` }>
+                    <Icon className={classes.vector} onClick={onClick}/>
+                    <img src={book.url}/>
+                    <div className={classes.info}>
+                        <div>
+                            <p className={classes.title}>{book.title}</p>
+                            <p className={classes.date}>{book.date}</p>
+                        </div>
+                        <div className={classes.pricel}>
+                            <p className={classes.price}>{book.price} с</p>
+                        </div>
+                    </div>
+                </div>
+            </li>
+        </ul>
+  </div>;
+};
+
+export default BookCardVersion2;
