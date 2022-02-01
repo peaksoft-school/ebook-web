385a12f3fb48a9fd0ff1cbaa8533292eda92565a
Lay7531 <ieye7531@gmail.com>
2022-02-01 14:19:31 +0600

    save

--- src/components/UI/Search/Search-icon/SearchIcon.js
+++ src/components/UI/Search/Search-icon/SearchIcon.js
@@ -1,10 +1,9 @@
-// import searchIcon from '../../../../assets/icons/searchIcon.svg'
+import {ReactComponent as SearchIconImage} from '../../../../assets/icons/searchIcon.svg'
 import classes from './Search-icon.module.css'
-import{ ReactComponent as OrangeSearchIcon } from '../../../../assets/icons/orangeSearchIcon.svg'
 
 const SearchIcon = ({ isActive }) => {
     const className = isActive ? classes.searchIconOrange : classes.searchIconGray
-  return <OrangeSearchIcon className={className} /> 
+  return <SearchIconImage className={className} /> 
 };
 
 export default SearchIcon;
