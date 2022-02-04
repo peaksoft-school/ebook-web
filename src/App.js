a61c8a6623b3332f6fb27e748e678d5eecf5ddcb
Zaryl-Zh <zaryl.abdrazakova@gmail.com>
2022-02-04 17:59:58 +0600

    done

--- src/App.js
+++ src/App.js
@@ -1,16 +1,8 @@
 import './App.css';
-import BookGenreDropdown from './components/BookCardRenderingDropdowns/BookGenreDropdown/BookGenreDropwdown';
-import BookStatusDropdown from './components/BookCardRenderingDropdowns/BookStatusDropdown/BookStatusDropdown';
-import BookTypeDropdown from './components/BookCardRenderingDropdowns/BookTypeDropdown/BookTypeDropdown';
 
 function App() {
   return (
-    <div className="App">
-      <BookStatusDropdown/>
-      <BookTypeDropdown/>
-
-      <BookGenreDropdown/>
-    </div>
+    <div className="App"></div>
   );
 }
 
