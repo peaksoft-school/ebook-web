5d1ce73d168dc9931827d97feef35d11d59a1352
KenzheSan <js3nursultan@gmial.com>
2022-02-11 19:44:37 +0600

    done

--- src/index.js
+++ src/index.js
@@ -1,32 +1,14 @@
-<<<<<<< HEAD
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
-=======
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import store from './store/store';
 import { Provider } from 'react-redux';
->>>>>>> 0547f701d491ca3a4035ebf2bd50258907e3b788
 import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import store from './store/store'
 
 ReactDOM.render(
-<<<<<<< HEAD
-	<React.StrictMode>
-		<Provider store={store}>
-			<BrowserRouter>
-				<App />
-			</BrowserRouter>
-		</Provider>
-	</React.StrictMode>,
-	document.getElementById('root'),
-)
-=======
   <React.StrictMode>
   <BrowserRouter>
   <Provider store={store}>
@@ -36,4 +18,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
->>>>>>> 0547f701d491ca3a4035ebf2bd50258907e3b788
