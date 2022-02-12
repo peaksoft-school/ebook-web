f06d2c13b944ff1cc239accf921591753d5cd735
KenzheSan <js3nursultan@gmial.com>
2022-02-12 14:16:48 +0600

    done

--- src/components/adminsBookCrat/booksList/BooksList.js
+++ src/components/adminsBookCrat/booksList/BooksList.js
@@ -3,12 +3,11 @@ import VendorBookCard from '../../UI/VendorBookCard/VendorBookCard'
 import classes from './BooksList.module.css'
 const BooksList = () => {
 	const books = useSelector((state) => state.bookCrat.books)
-	console.log(books);
 
 	return (
 		<div className={classes.bookslistbox}>
 			{books.map((item) => (
-				<VendorBookCard book={item} key={item.id}/>
+				<VendorBookCard book={item} key={item.id} />
 			))}
 		</div>
 	)
