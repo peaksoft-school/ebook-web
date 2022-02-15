549a1105e7886c6569aa8192b6223936cac0a3ac
Abubakir <uzbekovabubakir@gmail.com>
2022-02-15 18:11:30 +0300

    update folders

--- src/utils/constants/constants.js
+++ src/utils/constants/constants.js
@@ -74,3 +74,33 @@ export const Search = {
    orangeSearchIcon: ORANGESEARCH,
    searchIcon: SEARCH,
 }
+
+const IS_LOGIN = 'isLogin'
+const IS_VENDOR = 'isVendor'
+const IS_USER = 'isUser'
+const NAME = 'name'
+const EMAIL = 'email'
+const PASSWORD = 'password'
+const CONFIRMPASSWORD = 'confirmpassword'
+const PHONE = 'phone'
+const SURNAME = 'surname'
+const PICTURE = 'PICTURE'
+const IS_AUDIOBOOK = 'ISAUDIOBOOK'
+const IS_PAPPERBOOK = 'ISPAPPERBOOK'
+const IS_ELECTROBOOK = 'ISELECTROBOOK'
+
+export {
+   IS_LOGIN,
+   IS_USER,
+   IS_VENDOR,
+   NAME,
+   EMAIL,
+   PASSWORD,
+   CONFIRMPASSWORD,
+   PHONE,
+   SURNAME,
+   PICTURE,
+   IS_AUDIOBOOK,
+   IS_PAPPERBOOK,
+   IS_ELECTROBOOK,
+}
