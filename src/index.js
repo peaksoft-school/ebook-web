e1f5faba373a327fe71ae70f9b705c37133e06d8
Abubakir <uzbekovabubakir@gmail.com>
2022-02-15 10:26:57 +0300

    removed error

--- src/index.js
+++ src/index.js
@@ -7,12 +7,12 @@ import { Provider } from 'react-redux'
 import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.render(
-	<React.StrictMode>
-		<BrowserRouter>
-			<Provider store={store}>
-				<App />
-			</Provider>
-		</BrowserRouter>
-	</React.StrictMode>,
-	document.getElementById('root'),
+   <React.StrictMode>
+      <BrowserRouter>
+         <Provider store={store}>
+            <App />
+         </Provider>
+      </BrowserRouter>
+   </React.StrictMode>,
+   document.getElementById('root')
 )
