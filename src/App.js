1a8241228ebe9f247a070afdfe4689b81ac4e3e3
KenzheSan <js3nursultan@gmial.com>
2022-02-21 17:38:18 +0600

    exchange layout papper-book

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
-// import { useEffect } from 'react'
-// import { useDispatch } from 'react-redux'
-// import { asyncUpdateBreadcrumb } from './store/breadCrumbsSlice'
-// import { getFromLocalStorage } from './utils/helpers'
-// import { EBOOK_BREADCRUMBS } from './utils/constants/constants'
+import { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
+import { asyncUpdateBreadcrumb } from './store/breadCrumbsSlice'
+import { getFromLocalStorage } from './utils/helpers'
+import { EBOOK_BREADCRUMBS } from './utils/constants/constants'
 import AdminLayout from './components/admin/AdminLayot/AdminLayout'
-// import AuthModal from './components/auth/authModal/AuthModal'
-// import AddBookForm from './containers/admin/adminAddBook/addBookForm/AddBookForm'
 import './App.css'
 
 function App() {
-   // const dispatch = useDispatch()
+   const dispatch = useDispatch()
 
-   // useEffect(() => {
-   //    const breadcrumbs = getFromLocalStorage(EBOOK_BREADCRUMBS)
-   //    dispatch(asyncUpdateBreadcrumb(breadcrumbs))
-   // }, [])
+   useEffect(() => {
+      const breadcrumbs = getFromLocalStorage(EBOOK_BREADCRUMBS)
+      dispatch(asyncUpdateBreadcrumb(breadcrumbs))
+   }, [])
 
    return (
       <div className="App">
          <AdminLayout />
-         {/* <AuthModal /> */}
-         {/* <AddBookForm /> */}
       </div>
    )
 }
