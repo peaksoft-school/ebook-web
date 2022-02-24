1a3f92c701b170b4bbe4a0cc476369941d60257a
Lay7531 <ieye7531@gmail.com>
2022-02-24 14:13:48 +0600

    save

--- src/containers/client/ClientMainPage/BestsellerBooksSection/BestsellerBooksSection.js
+++ src/containers/client/ClientMainPage/BestsellerBooksSection/BestsellerBooksSection.js
@@ -1,7 +1,15 @@
-// import classes from './BestsellerBooksSection.module.css'
+import Button from '../../../../components/UI/Button/Button'
+import classes from './BestsellerBooksSection.module.css'
 
 const BestsellerBooksSection = () => {
-   return <div>BestsellerBooksSection</div>
+   return (
+      <div className={classes.bestsellerBooksSectionContainer}>
+         <div className={classes.sectionTopPart}>
+            <h2>Бестселлеры</h2>
+            <Button variant="aboutMoreBtn">Смотреть все</Button>
+         </div>
+      </div>
+   )
 }
 
 export default BestsellerBooksSection
