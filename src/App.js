16f1dd36da528762ec53a3221c01c8712c87d269
KenzheSan <js3nursultan@gmail.com>
2022-02-25 04:29:55 +0600

    Забыл очистить app.js

--- src/App.js
+++ src/App.js
@@ -2,10 +2,8 @@ import './App.css'
 import { useEffect } from 'react'
 import { useDispatch } from 'react-redux'
 import { asyncAutoUpdateBreadcrumb } from './store/breadCrumbsSlice'
-// import AdminRoutes from './routes/AdminRoutes'
+import AdminRoutes from './routes/AdminRoutes'
 import { asyncUpdateUserRole } from './store/userRoleSlice'
-import AddBookForm from './containers/admin/adminAddBook/addBookForm/AddBookForm'
-// import BooksCratLayout from './components/adminsBookCrat/BooksCrat'
 
 function App() {
    const dispatch = useDispatch()
@@ -17,9 +15,7 @@ function App() {
 
    return (
       <div className="App">
-         {/* <AdminRoutes /> */}
-         {/* <BooksCratLayout /> */}
-         <AddBookForm />
+         <AdminRoutes />
       </div>
    )
 }
