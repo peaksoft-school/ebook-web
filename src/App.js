cbf1a7a25d4bd7af1d08d20a96c8ff38a776342e
Lay7531 <ieye7531@gmail.com>
2022-02-26 18:22:05 +0600

    save

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
 import './App.css'
-// import { useEffect } from 'react'
-// import { useDispatch } from 'react-redux'
-// import { asyncAutoUpdateBreadcrumb } from './store/breadCrumbsSlice'
-// import AdminRoutes from './routes/AdminRoutes'
-// import { asyncUpdateUserRole } from './store/userRoleSlice'
-import VendorBookPage from './containers/vendor/VendorBookPage/VendorBookPage'
+import { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
+import { asyncAutoUpdateBreadcrumb } from './store/breadCrumbsSlice'
+import AdminRoutes from './routes/AdminRoutes'
+import { asyncUpdateUserRole } from './store/userRoleSlice'
 
 function App() {
-   // const dispatch = useDispatch()
+   const dispatch = useDispatch()
 
-   // useEffect(() => {
-   //    dispatch(asyncUpdateUserRole())
-   //    dispatch(asyncAutoUpdateBreadcrumb())
-   // }, [])
+   useEffect(() => {
+      dispatch(asyncUpdateUserRole())
+      dispatch(asyncAutoUpdateBreadcrumb())
+   }, [])
 
    return (
       <div className="App">
-         {/* <AdminRoutes /> */}
-         <VendorBookPage />
+         <AdminRoutes />
       </div>
    )
 }
