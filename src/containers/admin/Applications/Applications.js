02a47afff70aeeddce4941bcd3177afd14445531
Lay7531 <ieye7531@gmail.com>
2022-02-28 18:00:18 +0600

    save

---
+++ src/containers/admin/Applications/Applications.js
@@ -0,0 +1,7 @@
+import classes from './Applications.module.css'
+
+const Applications = () => {
+   return <div className={classes.applicationsContainer}>Applications</div>
+}
+
+export default Applications
