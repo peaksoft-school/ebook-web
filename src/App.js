269d0b94dedc66cf0f12d5f1f5b3bb6de2b3bd06
KenzheSan <js3nursultan@gmial.com>
2022-03-05 12:46:54 +0600

    Забыл апп почистить

--- src/App.js
+++ src/App.js
@@ -2,9 +2,8 @@ import './App.css'
 import { useEffect } from 'react'
 import { useDispatch } from 'react-redux'
 import { asyncAutoUpdateBreadcrumb } from './store/breadCrumbsSlice'
-// import AppRoutes from './routes/AppRoutes'
+import AppRoutes from './routes/AppRoutes'
 import { asyncUpdateUserRole } from './store/userRoleSlice'
-import VendorAddBookForm from './containers/vendor/vendorAddBookForm/addBookForm/VendorAddBookForm'
 
 function App() {
    const dispatch = useDispatch()
@@ -16,8 +15,7 @@ function App() {
 
    return (
       <div className="App">
-         {/* <AppRoutes /> */}
-         <VendorAddBookForm />
+         <AppRoutes />
       </div>
    )
 }
