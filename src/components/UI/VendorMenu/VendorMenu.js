eb9b4723f0bfcf219be06f9910045c87fb801fa3
Abubakir <uzbekovabubakir@gmail.com>
2022-03-07 14:53:21 +0300

    start

---
+++ src/components/UI/VendorMenu/VendorMenu.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import classes from './VendorMenu.module.css'
+
+const VendorMenu = () => {
+   return <div className={classes.container}>VendorMenu</div>
+}
+
+export default VendorMenu
--- src/components/vendor/VendorLayout/VendorLayout.js
+++ src/components/vendor/VendorLayout/VendorLayout.js
@@ -1,12 +1,14 @@
 import { Outlet } from 'react-router-dom'
 import VendorHeader from '../../../layout/headers/VendorHeader/VendorHeader'
 import VendorFooter from '../../../layout/VendorFooter/VendorFooter'
+import VendorMenu from '../../UI/VendorMenu/VendorMenu'
 import classes from './VendorLayout.module.css'
 
 const VendorLayout = () => {
    return (
       <div className={classes.vendorLayoutContainer}>
          <VendorHeader />
+         <VendorMenu />
          <Outlet />
          <VendorFooter />
       </div>
