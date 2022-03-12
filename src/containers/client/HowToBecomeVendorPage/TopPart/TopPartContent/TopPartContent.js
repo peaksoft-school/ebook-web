75bef93efa512b4e57dab3e7aeebe27eeec5e59f
Abubakir <uzbekovabubakir@gmail.com>
2022-03-12 11:50:59 +0300

    bug-fix

--- src/containers/client/HowToBecomeVendorPage/TopPart/TopPartContent/TopPartContent.js
+++ src/containers/client/HowToBecomeVendorPage/TopPart/TopPartContent/TopPartContent.js
@@ -1,9 +1,15 @@
+import { useState } from 'react'
 import classes from './TopPartContent.module.css'
 import Button from '../../../../../components/UI/Button/Button'
 import { ReactComponent as GirlSitOnBooksImage } from '../../../../../assets/icons/girlSitOnBook.svg'
 import { ReactComponent as PortalToBookWorldImage } from '../../../../../assets/icons/portalToBookWorld.svg'
+import AuthModal from '../../../../../components/auth/authModal/AuthModal'
 
 const TopPartContent = () => {
+   const [vendor, setVendor] = useState(false)
+   const HowToBecomeVendor = () => {
+      setVendor((vendor) => !vendor)
+   }
    return (
       <div className={classes.topPartContentContentContainer}>
          <div className={classes.textContainer}>
@@ -13,9 +19,14 @@ const TopPartContent = () => {
             <p className={classes.text}>
                Начните продавать свои книги на eBook
             </p>
-            <Button className={classes.button} variant="variantBecomeToSeller">
+            <Button
+               className={classes.button}
+               variant="variantBecomeToSeller"
+               onClick={HowToBecomeVendor}
+            >
                Стать продавцом
             </Button>
+            {vendor && <AuthModal onClose={HowToBecomeVendor}>a</AuthModal>}
          </div>
          <GirlSitOnBooksImage className={classes.girlSitOnBooksImage} />
       </div>
--- src/containers/client/PromoCodePage/PromoCodePage.js
+++ src/containers/client/PromoCodePage/PromoCodePage.js
@@ -1,19 +1,57 @@
+import { useState } from 'react'
 import { ReactComponent as PromoCode } from '../../../assets/icons/promocode.svg'
 import { promoBooks } from '../../../utils/constants/books'
+import { sendRequest } from '../../../utils/helpers'
 import classes from './PromoCode.module.css'
 import PromoCodeCard from './PromoCodeCard/PromoCodeCard'
+import Button from '../../../components/UI/Button/Button'
+import Modal from '../../../components/UI/modal-window/ModalWindow'
 
 const PromoCodePage = () => {
+   const [activePromo, setActivePromo] = useState('')
+   const [error, setError] = useState(false)
    const render = promoBooks.map((item) => {
       return <PromoCodeCard book={item} key={item.id} />
    })
+
+   const onChangePromoHandler = (e) => {
+      setActivePromo(e.target.value)
+   }
+
+   const onChangeErrorHandler = () => {
+      setError((error) => !error)
+   }
+
+   const foundPromoBooks = async () => {
+      try {
+         const configRequest = {
+            url: 'api/promo/find',
+            method: 'POST',
+            body: activePromo,
+         }
+         const response = await sendRequest(configRequest)
+         console.log(response)
+      } catch (error) {
+         onChangeErrorHandler()
+      }
+   }
+
    return (
       <div className={classes.promo}>
          <PromoCode />
          <span className={classes.title}>Активация промокода eBook</span>
          <div className={classes.textarea}>
-            <input className={classes.input} placeholder="Введите промокод" />
-            <button type="button" className={classes.button}>
+            <input
+               className={classes.input}
+               placeholder="Введите промокод"
+               value={activePromo}
+               onChange={onChangePromoHandler}
+            />
+            <button
+               type="button"
+               className={classes.button}
+               onClick={foundPromoBooks}
+            >
                Активировать
             </button>
          </div>
@@ -24,6 +62,16 @@ const PromoCodePage = () => {
             <p className={classes.foundBook}>Найдены 2344 книг</p>
             <div className={classes.result}>{render}</div>
          </div>
+         {error && (
+            <Modal onClose={onChangeErrorHandler} className={classes.modal}>
+               <div className={classes.text}>
+                  Введены неверные символы в коде купона{' '}
+                  <Button onClick={onChangeErrorHandler} className={classes.ok}>
+                     Ok
+                  </Button>
+               </div>
+            </Modal>
+         )}
       </div>
    )
 }
